becd1b16f242f3a2aa759397d60e4e299dd2b821
pranaavs2003 <pranaavs2003@gmail.com>
2026-04-26 22:49:14 +0530

    Added redis caching

--- src/services/shorten.service.ts
+++ src/services/shorten.service.ts
@@ -2,12 +2,15 @@ import { eq } from "drizzle-orm";
 import db from "../db";
 import { urlTable } from "../db/schema";
 import { generateShortUrl } from "../common/utils/generate-short-url";
+import { redis } from "../db/redis/redis";
 
 export const createUrl = async (data: any) => {
   try {
     const originalUrl = data.original_url;
 
     // Check if URL is in cache
+    const chachedValue = await redis.get(originalUrl);
+    if (chachedValue) return chachedValue;
 
     // Check if URL already exists
     const checkUrl = await db
@@ -18,21 +21,20 @@ export const createUrl = async (data: any) => {
     const doesUrlExist = checkUrl.length > 0;
 
     if (doesUrlExist) {
-      console.log("Fetched");
       // If URL already there in DB, send the fetched value
+      await redis.set(data.original_url, checkUrl[0].short_code);
       return checkUrl[0].short_code;
     } else {
-      console.log("Added");
       // If not add URL to the DB
       const shortCode = generateShortUrl(originalUrl);
       await db.insert(urlTable).values({
         original_url: originalUrl,
         short_code: shortCode,
       });
+      await redis.set(data.original_url, shortCode);
       return shortCode;
     }
   } catch (err) {
-    console.error(err);
     throw err;
   }
 };
